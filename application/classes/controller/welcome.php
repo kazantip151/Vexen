0fe76149bcd45fe486164a45c4a9b5064b11dded
Alex Pinsker <jeneia08@012.net.il>
2013-02-19 00:43:27 +0200

    make it work

---
+++ application/classes/controller/welcome.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Welcome extends Controller_Site {
+
+	public function action_index()
+	{
+
+	}
+
+} // End Controller_Welcome
