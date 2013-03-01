303cc050e44ae7ed1beb9b13466af166d995e520
Alex Pinsker <jeneia08@012.net.il>
2013-03-02 00:23:47 +0200

    VEX-1 Changed branch

--- application/classes/controller/user.php
+++ application/classes/controller/user.php
@@ -13,7 +13,43 @@ class Controller_User extends Controller_Site {
 
 	public function action_registration()
 	{
+		$post = $this->request->post();
 
+		if ($post)
+		{
+			try
+			{
+				// Load users
+				$user = new Model_User();
+				$time = time();
+
+				// Set values for POST
+				$post = arr::extract($this->request->post(), array('user_name', 'password', 'token', 'created'));
+				$post = arr::map('trim', $post);
+
+				// Save job
+				$user = $user->save();
+
+				// Success message
+				Notices::add('success', 'Well done! '.($user ? 'updated' : 'created').' successfuly!');
+
+				$this->request->redirect();
+			}
+			catch (ORM_Validation_Exception $e)
+			{
+				Notices::add('error', $e->errors(''));
+			}
+		}
+		else
+		{
+//			if ($job_id)
+//			{
+//				$job = new Model_Job($job_id);
+//				$post = $job->as_array();
+//			}
+		}
+
+//		$this->view->job = $post;
 	}
 
 } // End Controller_User
